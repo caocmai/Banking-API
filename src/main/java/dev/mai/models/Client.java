daf532b2cb57cc97f7635fc7901bb7d60911564e
caocmai <caomai@rocketmail.com>
2021-06-30 22:14:20 -0500

    Add getters and setters to Client model

--- src/main/java/dev/mai/models/Client.java
+++ src/main/java/dev/mai/models/Client.java
@@ -1,5 +1,67 @@
 package dev.mai.models;
 
 public class Client {
+	private int id;
+	private String firstName;
+	private String lastName;
+	
+	
+	public Client() {
+		super();
+	}
+
+
+	public Client(int id, String firstName, String lastName) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+
+	public Client(String firstName, String lastName) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+
+	public String getLastName() {
+		return lastName;
+	}
+
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Client [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + "]";
+	}
+	
+	
+	
 
 }
