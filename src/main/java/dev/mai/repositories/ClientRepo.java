0dc1e703eaa745558905d423a02f1964b9920ccb
caocmai <caomai@rocketmail.com>
2021-06-30 22:23:03 -0500

    Add Interface to implement CRUD on Client

---
+++ src/main/java/dev/mai/repositories/ClientRepo.java
@@ -0,0 +1,20 @@
+package dev.mai.repositories;
+
+import java.util.List;
+
+import dev.mai.models.Client;
+
+
+public interface ClientRepo {
+	
+	public Client getClient(int id);
+	
+	public List<Client> getAllClients();
+	
+	public Client addClient(Client m); 
+	
+	public Client updateClient(Client changeClient);
+	
+	public Client deleteClient(int id);
+
+}
