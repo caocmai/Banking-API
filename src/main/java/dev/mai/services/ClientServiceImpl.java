72bc41ac5b36e92b739573efff29e642824521a4
caocmai <caomai@rocketmail.com>
2021-07-02 08:26:06 -0500

    Implement ClientService interface

---
+++ src/main/java/dev/mai/services/ClientServiceImpl.java
@@ -0,0 +1,35 @@
+package dev.mai.services;
+
+import java.util.List;
+
+import dev.mai.models.Client;
+import dev.mai.repositories.ClientRepo;
+
+public class ClientServiceImpl implements ClientRepo {
+
+	@Override
+	public Client getClient(int id) {
+		return null;
+	}
+
+	@Override
+	public List<Client> getAllClients() {
+		return null;
+	}
+
+	@Override
+	public Client addClient(Client m) {
+		return null;
+	}
+
+	@Override
+	public Client updateClient(Client changeClient) {
+		return null;
+	}
+
+	@Override
+	public Client deleteClient(int id) {
+		return null;
+	}
+
+}
