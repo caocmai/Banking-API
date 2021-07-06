492294a94a2f0fcbc3180b10d38f3a40052f69f2
caocmai <caomai@rocketmail.com>
2021-07-05 22:19:21 -0700

    Add Client Controller

---
+++ src/main/java/dev/mai/controllers/ClientController.java
@@ -0,0 +1,49 @@
+package dev.mai.controllers;
+
+import java.util.List;
+
+import com.google.gson.Gson;
+
+import dev.mai.models.Client;
+import dev.mai.services.ClientService;
+import io.javalin.http.Handler;
+
+public class ClientController {
+	private ClientService cs;
+	Gson gson = new Gson();
+
+	public ClientController(ClientService cs) {
+		this.cs = cs;
+	}
+	
+	public Handler getAllMovies = (context) -> {
+		List<Client> movies = cs.getAllClients();
+		
+		context.result(gson.toJson(movies));
+	};
+	
+	
+	public Handler getClientById = (ctx) -> {
+		String input = ctx.pathParam("movieId");
+		int id;
+		
+		if (input.matches("^-?[0-9]+")) { // Regex
+			id = Integer.parseInt(input);
+			
+		} else {
+			id = -1;
+		}
+		
+		
+		Client client = cs.getClient(id);
+		
+		if (client != null ) {
+			ctx.result(gson.toJson(client));
+		} else {
+			ctx.status(400);
+		}
+		
+		ctx.result((client != null) ? gson.toJson(client) : "{}");
+
+	};
+}
